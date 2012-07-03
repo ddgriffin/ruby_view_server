10e603e1b44afbfc11d33c5a5618bcd8be157a63
ddgriffin <user@server.com>
2012-07-02 20:13:50 -0500

    week3 exercise step 1

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,18 @@
 require 'erb'
 
-x = 42
-template = ERB.new "The value of x is: <%= x %>"
+request = "Tell us the answer to life, the universe, everything!"
+template = ERB.new "They said: <%= request %>"
 puts template.result(binding)
+
+answer = 42
+template = ERB.new "The answer is: <%= answer %>"
+puts template.result(binding)
+
+question = "Wait... What is the question?"
+template = ERB.new "They asked: <%= question %>"
+puts template.result(binding)
+
+response = "I don't know."
+template = ERB.new "The computer said: <%= response %>"
+puts template.result(binding)
+
